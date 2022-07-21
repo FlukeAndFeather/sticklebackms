8d2842934487b64925fdc1ac2213a9e83cd818ad
Max Czapanskiy <maxczap@stanford.edu>
2022-07-21 10:32:10 -0700

    Just a checkpoint, still coding

---
+++ analysis/scratch/runstickleback.R
@@ -0,0 +1,195 @@
+library(moments)
+library(ranger)
+library(RcppRoll)
+library(rstickleback)
+library(tidyverse)
+library(zoo)
+
+# Read data ---------------------------------------------------------------
+
+sensors <- arrow::read_parquet("analysis/data/raw_data/sensors.parquet")
+events <- arrow::read_parquet("analysis/data/raw_data/events.parquet") %>%
+  filter(event == "lunge")
+
+# Run cross validation ----------------------------------------------------
+
+## Split data into train/test ---------------------------------------------
+
+split_data <- function(sensors, events) {
+  # Randomly sample up to two hours of data from five deployments
+  deployids <- sample(unique(events$deployid), size = 5, replace = FALSE)
+  get_start <- function(deployid) {
+    e_t <- events$datetime[events$deployid == deployid]
+    # Assuming a max 40 s window, a 21 s buffer ensures at least one event
+    # falls within the sample window
+    result <- sample(e_t, 1) - runif(1, 21, 2 * 3600 - 21)
+  }
+  start_times <- map(deployids, get_start)
+  names(start_times) <- deployids
+  train_sensors <- map_dfr(deployids, function(id) {
+    start_time <- start_times[[id]]
+    sensors %>%
+      filter(deployid == id,
+             datetime >= start_time,
+             datetime <= start_time + 3600 * 2)
+  })
+  set_nearest <- function(x, y) {
+    # Assumes both are sorted
+    stopifnot(!is.unsorted(x),
+              !is.unsorted(y))
+    # Assumes x within y
+    stopifnot(x[1] > y[1],
+              x[length(x)] < y[length(y)])
+    y[findInterval(x, y)]
+  }
+  train_events <- map_dfr(deployids, function(id) {
+    start_time <- start_times[[id]]
+    valid_times <- sensors$datetime[sensors$deployid == id]
+    events %>%
+      filter(deployid == id,
+             # Filter out events near boundaries
+             datetime > start_time + 20,
+             datetime < start_time + 3600 * 2 - 20) %>%
+      mutate(datetime = set_nearest(datetime, valid_times))
+  })
+
+  test_sensors <- anti_join(sensors, train_sensors, by = "deployid")
+  test_events <- anti_join(sensors, train_sensors, by = "deployid") %>%
+    group_by(deployid) %>%
+    mutate(
+      datetime = set_nearest(
+        datetime,
+        test_sensors$datetime[test_sensors$deployid == deployid[1]]
+      )
+    )
+  list(train_sensors = train_sensors,
+       train_events = train_events,
+       test_sensors = test_sensors,
+       test_events = test_events)
+}
+
+## Utility functions ------------------------------------------------------
+
+create_features <- function(sensors) {
+  roll_fn <- function(fn) {
+    function(x) fn(x, n = 400, fill = NA, align = "center")
+  }
+  roll_fn2 <- function(fn) {
+    function(x) rollapply(x, width = 400, FUN = fn, fill = NA, align = "center")
+  }
+  stat_fns <- list(
+    min = roll_fn(roll_min),
+    max = roll_fn(roll_max),
+    mean = roll_fn(roll_mean),
+    sd = roll_fn(roll_sd),
+    skew = roll_fn2(skewness),
+    kurt = roll_fn2(kurtosis)
+  )
+  features <- sensors %>%
+    group_by(deployid) %>%
+    mutate(across(c(depth, pitch, roll, speed),
+                  stat_fns)) %>%
+    left_join(events, by = c("deployid", "datetime")) %>%
+    mutate(event = factor(ifelse(is.na(event), "non-event", "event"))) %>%
+    drop_na()
+}
+
+durations <- function(sensors) {
+  sensors %>%
+    group_by(deployid) %>%
+    summarize(d = max(datetime) - min(datetime)) %>%
+    pull(d)
+}
+
+f1 <- function(tp, fp, fn) {
+  tp / (tp + 1/2 * (fp + fn))
+}
+
+delta_r <- function(tp, fp, fn, d) {
+  r = (tp + fn) / sum(d)
+  r_hat = (tp + fp) / sum(d)
+  abs((r - r_hat) / r)
+}
+
+## Train models -----------------------------------------------------------
+
+train_stickleback <- function(sensors, events) {
+  sensors <- Sensors(sensors,
+                     "deployid",
+                     "datetime",
+                     c("depth", "pitch", "roll", "speed"))
+  events <- Events(events,
+                   "deployid",
+                   "datetime")
+  tsc <- compose_tsc(module = "interval_based",
+                     algorithm = "TimeSeriesForestClassifier",
+                     params = list(n_estimators = 8L),
+                     columns = columns(train_sensors))
+  sb <- Stickleback(tsc,
+                    win_size = 400,
+                    tol = 5,
+                    nth = 5,
+                    n_folds = 4)
+  sb_fit(sb, sensors, events)
+  sb
+}
+
+train_randomforest <- function(sensors, events) {
+  features <- create_features(sensors)
+  feat_cols <- colnames(features)[grepl(".*_.*", colnames(features))]
+  rf_form <- as.formula(sprintf("event ~ %s",
+                                paste(feat_cols, collapse = "+")))
+  ranger(rf_form, features, num.trees = 200)
+}
+
+test_stickleback <- function(m, sensors, events) {
+  sensors <- Sensors(sensors,
+                     "deployid",
+                     "datetime",
+                     c("depth", "pitch", "roll", "speed"))
+  events <- Events(events,
+                   "deployid",
+                   "datetime")
+
+  p <- sb_predict(m, sensors)
+  o <- sb_assess(m, p, events)@.data %>%
+    as.data.frame() %>%
+    group_by(deployid) %>%
+    summarize(tp = sum(outcome == "TP"),
+              fp = sum(outcome == "FP"),
+              fn = sum(outcome == "FN"))
+
+  list(f1 = with(o, f1(tp, fp, fn)),
+       delta_r = with(o, delta_r(tp, fp, fn, durations(sensors))))
+}
+
+test_randomforest<- function(m, sensors, events) {
+  features <- create_features(sensors)
+
+  p <- predict(m, features)
+  o <- sb_assess(m, p, events)@.data %>%
+    as.data.frame() %>%
+    group_by(deployid) %>%
+    summarize(tp = sum(outcome == "TP"),
+              fp = sum(outcome == "FP"),
+              fn = sum(outcome == "FN"))
+
+  list(f1 = with(o, f1(tp, fp, fn)),
+       delta_r = with(o, delta_r(tp, fp, fn, durations)))
+}
+
+run_cv <- function(sensors, events) {
+  train_test <- split_data(sensors, events)
+
+  # Train models
+  sb <- train_stickleback(train_test$train_sensors, train_test$train_events)
+  rf <- train_randomforest(train_test$train_sensors, train_test$train_events)
+
+  # Make and assess predictions
+  sb_results <- test_stickleback(sb,
+                                 train_test$test_sensors,
+                                 train_test$test_events)
+  rf_results <- test_randomforest(rf,
+                                  train_test$test_sensors,
+                                  train_test$test_events)
+}
