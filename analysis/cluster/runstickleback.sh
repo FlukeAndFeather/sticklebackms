e0df211ad1c8ab2486b7cdee14ee735ba6d68dea
Max Czapanskiy <maxczap@stanford.edu>
2022-07-25 13:25:51 -0700

    prepare for slurm!

--- analysis/cluster/runstickleback.sh
+++ analysis/cluster/runstickleback.sh
@@ -2,30 +2,17 @@
 #
 #SBATCH --job-name=runstickleback
 #
-#SBATCH --time=16:00:00
+#SBATCH --time=24:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=20
-#SBATCH --mem-per-cpu=6G
+#SBATCH --cpus-per-task=10
+#SBATCH --mem-per-cpu=8G
 #SBATCH --mail-type=ALL
 
+# All the stickleback (Py) and sticklbackms (R) dependencies, plus devtools (R)
+# have to be installed first!
+
 cd $HOME/repos/sticklebackms
-ml R
+ml R/4.1
 ml python/3.9
 
-SIF=$GROUP_HOME/$USER/sing/sb-test_latest.sif
-SCRIPT=stickleback_test.py
-DATA=$SCRATCH/stickleback/bw_breaths_sensors_events.pkl
-WINDOW=50
-FOLDS=2
-TREES=64
-
-ml python/3.9
-cd $HOME/repos/run-stickleback
-echo WINDOW=$WINDOW FOLDS=$FOLDS TREES=$TREES
-python3 $SCRIPT $DATA $WINDOW $FOLDS $TREES $SLURM_CPUS_PER_TASK
-
-# Verify writable directories exist
-# mkdir -p $SCRATCH/.numba_cache
-# MPLCONFIGDIR=$SCRATCH/.mplconfig
-
-# singularity exec --env-file stickleback_env $SIF python3 $SCRIPT $DATA $WINDOW $FOLDS $TREES $SLURM_CPUS_PER_TASK
+Rscript analysis/cluster/runstickleback.R 200 5 8 2 8 2 8 200 10 10
