558a039fe6763f67a3366f8fe44251d206221cc8
Max Czapanskiy <maxczap@stanford.edu>
2022-07-26 22:25:57 -0700

    Shorten lunge window

--- analysis/cluster/runstickleback.sh
+++ analysis/cluster/runstickleback.sh
@@ -15,4 +15,4 @@ cd $HOME/repos/sticklebackms
 ml R/4.1
 ml python/3.9
 
-Rscript analysis/cluster/runstickleback.R breath 60 3 8 2 8 4 8 200 20 20
+Rscript analysis/cluster/runstickleback.R lunge 50 5 8 2 8 4 8 200 20 20
