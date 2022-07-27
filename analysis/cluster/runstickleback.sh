343f57fcde328b65869724f78e8d46bdbe00a949
Max Czapanskiy <maxczap@stanford.edu>
2022-07-26 18:37:45 -0700

    Update runstickleback.sh

--- analysis/cluster/runstickleback.sh
+++ analysis/cluster/runstickleback.sh
@@ -15,4 +15,4 @@ cd $HOME/repos/sticklebackms
 ml R/4.1
 ml python/3.9
 
-Rscript analysis/cluster/runstickleback.R 150 5 8 2 4 2 8 200 10 10
+Rscript analysis/cluster/runstickleback.R breath 60 3 8 2 8 4 8 200 20 20
