57fc2c2b3ba0c316eee0b78387c931ec5c405899
Max Czapanskiy <maxczap@stanford.edu>
2022-07-30 12:02:46 -0700

    Run breaths analysis

--- analysis/cluster/runstickleback.sh
+++ analysis/cluster/runstickleback.sh
@@ -2,17 +2,17 @@
 #
 #SBATCH --job-name=runstickleback
 #
-#SBATCH --time=24:00:00
+#SBATCH --time=6:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=10
-#SBATCH --mem-per-cpu=8G
+#SBATCH --cpus-per-task=20
+#SBATCH --mem-per-cpu=4G
 #SBATCH --mail-type=ALL
 
 # All the stickleback (Py) and sticklbackms (R) dependencies, plus devtools (R)
 # have to be installed first!
 
-cd $HOME/repos/sticklebackms
+cd $SCRATCH/repos/sticklebackms
 ml R/4.1
 ml python/3.9
 
-Rscript analysis/cluster/runstickleback.R lunge 80 5 8 2 8 4 8 200 20 20
+Rscript analysis/cluster/runstickleback.R breath 60 3 8 2 8 4 8 200 20 20
